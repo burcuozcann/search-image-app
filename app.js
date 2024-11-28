450a16cf12339552b052b7866a4bc2c63dad9b47
Burcu Ozcan <burcuozcann91@gmail.com>
2024-11-28 23:58:07 +0000

    Add Unsplash API integration for photo search
    This commit introduces a search function that integrates with the Unsplash API to fetch photos based on user input

--- app.js
+++ app.js
@@ -14,7 +14,16 @@ function runEventListener(){
 }
 
 function search(e){
-   const value=searchInput.value.trim();
+    const value=searchInput.value.trim();
+    fetch(`https://api.unsplash.com/search/photos?query=${value}`,{
+         method:"GET",
+         headers:{
+            Authorization:"Client-ID GrGGBxMI35Q_Oz79uSL5FZX7YpOhbEl9SGdT6nsWNvE"
+         }
+    })
+   .then((res)=>res.json())
+   .then((data)=>console.log(data))
+   .catch((err)=>console.log(err));
 
     e.preventDefault();
 
